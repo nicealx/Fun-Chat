79db335f9badec855cd810b2a8d1f4e6f1c2c3ea
Aleksei Kurtukov <kav.prok@gmail.com>
2024-04-23 02:20:45 +0700

    refactor: edit about text

--- src/components/view/about/about-view.ts
+++ src/components/view/about/about-view.ts
@@ -3,6 +3,9 @@ import ElementCreator from '../../../utils/element-creator';
 import Component from '../../../utils/component';
 import './about.css';
 
+const ABOUT_TEXT =
+  'The Fun Chat application was developed as part of the RSSchool JS/FE 2023Q4 course.';
+
 export default class AboutView extends Component {
   private title: ElementCreator;
 
@@ -12,8 +15,8 @@ export default class AboutView extends Component {
 
   constructor(tag: string, className: string) {
     super(tag, className);
-    this.title = new ElementCreator('h2', 'about__title', 'About App');
-    this.text = new ElementCreator('p', 'about__text', 'Some text');
+    this.title = new ElementCreator('h2', 'about__title', 'About Fun Chat App');
+    this.text = new ElementCreator('p', 'about__text', ABOUT_TEXT);
     this.button = new ButtonCreator('btn about__btn', 'button', 'Back', false);
     this.addCallbacks();
     this.createView();
